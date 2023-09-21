395e5ca8ef2d96f685fee61f32c8d40b2b5ee578
binayrijal <binayrijal22@gmail.com>
2023-09-21 18:07:48 +0545

    type used

--- env/main.py
+++ env/main.py
@@ -5,3 +5,9 @@ app=FastAPI()
 @app.get("/")
 async def root():
     return {"message":"helloworld"}
+#function defination
+@app.get("/name")
+def my_name(firstname:str,lastname:str):
+    full_name=firstname.title()+""+lastname.title()
+    return (full_name)
+print(my_name("Binay","Rijal"))
